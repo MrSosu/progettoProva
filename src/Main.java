a08d29f36814e610a8d5a10df510ec5e386041b3
MrSosu <adriani.marco.93@gmail.com>
2022-09-06 11:43:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci una parola");
+        String parola = input.nextLine();
+        System.out.println("La parola e' lunga: " + parola.length());
+    }
+
+}
